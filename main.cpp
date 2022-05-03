8f09f7448653b27c821fc38249be0de588520fa5
wangyihu2020 <82321180+wangyihu2020@users.noreply.github.com>
2022-05-03 16:26:07 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,44 +1,45 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
 
-
-std::vector<int> getArray(std::string& s){
-    std::vector<int> aux(s.size(), 0);
-    aux[0] = -1;
-    aux[1] = 0;
-    int cn = 0;
-    for(int i = 2; i < s.size(); ){
-        if(s[i - 1] == s[cn]) aux[i++] = ++cn;
-        else if(cn != 0) cn = aux[cn];
-        else i++;
+using namespace std;
+int check(string& s){
+    string aux(2 * s.size() + 1, '0');
+    int index = 0;
+    for(char& c : aux){
+        c = (index % 2 == 0) ? '#' : s[index / 2];
+        index++;
     }
-    return aux;
-}
-int checkifinclude(std::string& S, std::string& s){
-    std::vector<int> s_array(getArray(s));
+
+    vector<int> radius(aux.size());
+    int R = -1;
     int i = 0;
-    int cn = 0;
-    while(i < S.size() && cn < s.size()){
-        if(s[cn] == S[i]){
-            cn++;
-            i++;
+    int C = -1;
+    int maxVal = 0;
+    while(i < radius.size()){
+        radius[i] = i >= R ? 1 : min(R - i, radius[2 * C - i]);
+        while(i + radius[i] < radius.size() && i - radius[i] >= 0){
+            if(aux[i + radius[i]] != aux[i - radius[i]])
+            {
+                maxVal = max(radius[i], maxVal);
+                break;
+            }
+            else {
+                radius[i]++;
+            }
+            if(i + radius[i] > R){
+                R = i + radius[i];
+                C = i;
+            }
+            maxVal = max(radius[i], maxVal);
         }
-        else if(cn != 0){
-            cn = s_array[cn];
-        }
-        else i++;
+        i++;
     }
-    if(cn < s.size() )
-        return -1;
-    else return i - cn;
+    return maxVal - 1;
 }
 int main() {
-    std::string S("abcdefabcdefabcdfg");
-    std::string s("g");
-    int res = checkifinclude(S, s);
-
-
+    string s("abcdcbasshwsssssssss");
+    int max = check(s);
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
